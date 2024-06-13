54333b7ad307fc0276123466886f708d027b4e17
Preethi Ravi <preethi.ravikumar78@gmail.com>
2024-06-13 10:31:46 -0400

    printing

--- check.py
+++
@@ -1,9 +0,0 @@
-import os
-import pandas as pd
-
-file_path = r"C:\Users\preet\OneDrive\Desktop\Preethi's Dashboard\employee_time_cards.csv"
-
-if os.path.exists(file_path):
-    df = pd.read_csv(file_path)
-else:
-    print(f"File not found: {file_path}")
